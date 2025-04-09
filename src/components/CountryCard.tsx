c9044fb1d2b57c50f1b67dbfd0859999dbd882b0
HeberRios <hebera.riosg@gmail.com>
2025-04-08 19:45:43 -0600

    Create the country card component
    The card component for the list of countries was created and styled, now
    in the MainContent component a country list was added and every country
    is a CountryCard component.

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,42 @@
+import '../styles/CountryCard.css';
+
+export type CountryCardProps = {
+  name: string;
+  capital: string[] | undefined;
+  region: string;
+  population: number;
+  flagImg: string;
+  flagAlt: string;
+};
+
+export function CountryCard({
+  name,
+  capital,
+  region,
+  population,
+  flagImg,
+  flagAlt,
+}: CountryCardProps) {
+  return (
+    <article className='country-card'>
+      <header>
+        <img src={flagImg} alt={flagAlt} />
+      </header>
+      <section className='country-card-text-container'>
+        <h2>{name}</h2>
+        <p>
+          <strong>population:</strong>
+          {population.toLocaleString()}
+        </p>
+        <p>
+          <strong>region:</strong>
+          {region}
+        </p>
+        <p>
+          <strong>capital:</strong>
+          {capital}
+        </p>
+      </section>
+    </article>
+  );
+}
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,10 +1,42 @@
 import '../styles/MainContent.css';
 import { SearchAndFilter } from './SearchAndFilter';
+import allCountriesData from '../data-mockups/data-v3_1.json';
+import { CountryCard } from './CountryCard';
+
+function formatCountryData(countriesData) {
+  return countriesData.map((country) => {
+    return {
+      name: country.name.official,
+      capital: country.capital,
+      region: country.region,
+      population: country.population,
+      flagImg: country.flags.svg,
+      flagAlt: country.flags.alt,
+    };
+  });
+}
+
+const formattedCountryData = formatCountryData(allCountriesData);
 
 export function MainContent() {
   return (
     <main className='page-content'>
       <SearchAndFilter />
+      <div className='countries-cards-container'>
+        {formattedCountryData.map((country) => {
+          return (
+            <CountryCard
+              key={country.name}
+              name={country.name}
+              capital={country.capital}
+              region={country.region}
+              population={country.population}
+              flagImg={country.flagImg}
+              flagAlt={country.flagAlt}
+            />
+          );
+        })}
+      </div>
     </main>
   );
 }
