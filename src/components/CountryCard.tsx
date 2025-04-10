c1836e862e07b2cf55498a6c2eeb06bd8400be87
HeberRios <hebera.riosg@gmail.com>
2025-04-10 13:54:08 -0600

    Wrap the country card component in a link
    We use a react router Link component to wrap the CountryCard component,
    this to navigate to the respective country details page.

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import '../styles/CountryCard.css';
 import { CountryCardProps } from '../types/types';
 
@@ -11,25 +12,27 @@ export function CountryCard({
   flagAlt,
 }: CountryCardProps) {
   return (
-    <article className='country-card'>
-      <header>
-        <img src={flagImg} alt={flagAlt} />
-      </header>
-      <section className='country-card-text-container'>
-        <h2>{name}</h2>
-        <p>
-          <strong>population:</strong>
-          {population.toLocaleString()}
-        </p>
-        <p>
-          <strong>region:</strong>
-          {region}
-        </p>
-        <p>
-          <strong>capital:</strong>
-          {capital}
-        </p>
-      </section>
-    </article>
+    <Link to={cca3} className='country-card-link'>
+      <article className='country-card'>
+        <header>
+          <img src={flagImg} alt={flagAlt} />
+        </header>
+        <section className='country-card-text-container'>
+          <h2>{name}</h2>
+          <p>
+            <strong>population:</strong>
+            {population.toLocaleString()}
+          </p>
+          <p>
+            <strong>region:</strong>
+            {region}
+          </p>
+          <p>
+            <strong>capital:</strong>
+            {capital}
+          </p>
+        </section>
+      </article>
+    </Link>
   );
 }
