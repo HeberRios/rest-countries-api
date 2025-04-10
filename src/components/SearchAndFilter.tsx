a0dea631f2390030be1c7e397ebb9de2b541e320
HeberRios <hebera.riosg@gmail.com>
2025-04-10 12:36:49 -0600

    Create a responsive grid for the countries list
    The countries list now display a row up to 3 elements, depending on the '
    width of the window, also the order of the elements in the search bar
    was modified, this to have a better UX when using the keyboard to
    navigate.

--- src/components/SearchAndFilter.tsx
+++ src/components/SearchAndFilter.tsx
@@ -7,9 +7,6 @@ export function SearchAndFilter() {
     <div className='container search-filter-section'>
       <div className='search-bar-container'>
         <form action='' className='country-search-form'>
-          <button type='submit' className='btn search-bar-btn'>
-            <SearchIcon />
-          </button>
           <input
             type='text'
             name=''
@@ -17,6 +14,10 @@ export function SearchAndFilter() {
             className='search-bar'
             placeholder='Search for a country...'
           />
+
+          <button type='submit' className='btn search-bar-btn'>
+            <SearchIcon />
+          </button>
         </form>
       </div>
 
@@ -31,7 +32,7 @@ export function SearchAndFilter() {
           <option value='europe'>Europe</option>
           <option value='oceania'>Oceania</option>
         </select>
-				<ArrowDownIcon/>
+        <ArrowDownIcon />
       </div>
     </div>
   );
