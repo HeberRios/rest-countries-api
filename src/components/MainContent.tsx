7eeb91af31a964d06545ed70daa5e65d5eaf33eb
HeberRios <hebera.riosg@gmail.com>
2025-04-14 15:44:50 -0600

    Implement useEffect to get the countries data

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,18 +3,25 @@ import { SearchAndFilter } from './SearchAndFilter';
 import allCountriesData from '../data-mockups/data-v3_1.json';
 import { formatCountryData } from '../utils/utils';
 import { CountriesList } from './CountriesList';
+import { useEffect, useState } from 'react';
+import { CountryCardProps } from '../types/types';
 
 // endpoint for the countries data :
 // https://restcountries.com/v3.1/all?fields=,cca3,name,capital,region,subregion,population,flags,tld,currencies,languages,borders
 
-const formattedCountryData = formatCountryData(allCountriesData);
-
 export function MainContent() {
+  const [countries, setCountriesData] = useState<CountryCardProps[]>([]);
+
+  useEffect(() => {
+    const formattedCountryData = formatCountryData(allCountriesData);
+    setCountriesData(formattedCountryData);
+  }, []);
+
   return (
     <main className='home-page-content'>
       <SearchAndFilter />
 
-      <CountriesList countriesData={formattedCountryData} />
+      <CountriesList countriesData={countries} />
     </main>
   );
 }
